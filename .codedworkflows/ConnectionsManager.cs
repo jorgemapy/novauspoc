622868bcd41b6beded22baf846a13b7af32e83db
Jorge Rincon <jorge@novaus.io>
2025-10-28 08:46:03 -0700

    New project

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace NovaDemoTestSuite_Tests
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
